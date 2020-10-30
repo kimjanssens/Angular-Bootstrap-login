922892862b331c8a363e010d35ed83937171d7f9
Kim <kimjanssens21@gmail.com>
2020-10-30 22:30:07 +0100

    feat: less validation

--- src/app/account/components/login/login.component.ts
+++ src/app/account/components/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -18,12 +18,7 @@ export class LoginComponent implements OnInit {
   public ngOnInit(): void {
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
-      ['current-password']: ['', [
-        Validators.required,
-        Validators.minLength(8),
-        lowercaseValidator,
-        uppercaseValidator
-      ]]
+      ['current-password']: ['', [Validators.required]]
     });
   }
 
@@ -35,27 +30,5 @@ export class LoginComponent implements OnInit {
     this.submitted = true;
 
     console.log(this.loginForm.value);
-  }
-}
-
-/** A password must contain at least one lowercase letter */
-export function lowercaseValidator(c: FormControl): { [key: string]: boolean | null } {
-  const regex = /[a-z]/g;
-
-  if (regex.test(c.value)) {
-    return null;
-  } else {
-    return { lowercase: true };
-  }
-}
-
-/** A password must contain at least one uppercase letter */
-export function uppercaseValidator(c: FormControl): { [key: string]: boolean | null } {
-  const regex = /[A-Z]/g;
-
-  if (regex.test(c.value)) {
-    return null;
-  } else {
-    return { uppercase: true };
   }
 }
